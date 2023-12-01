fc4cedbcc852c46fd85bdefe6ac9c94525084ae4
aliaksei tsimoshchanka <timoschenko.aleksey@gmail.com>
2023-12-01 16:14:34 +0300

    update apiPaths for products and import

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://d7km2x2u1j.execute-api.eu-north-1.amazonaws.com",
+  product: "https://68t64o1otl.execute-api.eu-north-1.amazonaws.com/",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://f3izpbp52d.execute-api.eu-north-1.amazonaws.com/",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
